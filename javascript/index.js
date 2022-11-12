21ec4715b5c36f732fd5084bd12a6042990982f6
Natalia Kur <63752791+Katalia91@users.noreply.github.com>
2022-11-12 22:16:54 +0100

    Display current location time

--- javascript/index.js
+++ javascript/index.js
@@ -33,6 +33,9 @@ sydneyTimeElement.innerHTML = sydneyTime.format('hh:mm:ss [<small>]A[</small>]')
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");

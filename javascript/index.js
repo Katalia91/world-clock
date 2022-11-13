924f9baba0837a28580e7c9dc4a388012c52a4aa
Natalia Kur <63752791+Katalia91@users.noreply.github.com>
2022-11-13 21:17:48 +0100

    Improve updateTime function

--- javascript/index.js
+++ javascript/index.js
@@ -2,27 +2,31 @@ function updateTime() {
 // Los Angeles
 
 let losAngelesElement = document.querySelector("#los-angeles");
+if (losAngelesElement) {
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format('hh:mm:ss [<small>]A[</small>]');
+}
 
 // Warsaw
 
 let warsawElement = document.querySelector("#warsaw");
+if (warsawElement) {
 let warsawDateElement = warsawElement.querySelector(".date");
 let warsawTimeElement = warsawElement.querySelector(".time");
 let warsawTime= moment().tz("Europe/Warsaw");
 
 warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
 warsawTimeElement.innerHTML = warsawTime.format('hh:mm:ss [<small>]A[</small>]');
-
+}
 
 // Sydney
 
 let sydneyElement = document.querySelector("#sydney");
+if (sydneyElement) {
 let sydneyDateElement = sydneyElement.querySelector(".date");
 let sydneyTimeElement = sydneyElement.querySelector(".time");
 let sydneyTime= moment().tz("Australia/Sydney");
@@ -30,7 +34,7 @@ let sydneyTime= moment().tz("Australia/Sydney");
 sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
 sydneyTimeElement.innerHTML = sydneyTime.format('hh:mm:ss [<small>]A[</small>]');
 }
-
+}
 function updateCity(event) {
     let cityTimeZone = event.target.value;
     if (cityTimeZone === "current") {
@@ -64,3 +68,6 @@ function changeTheme () {
 }
 let themeButton = document.querySelector(".theme-button");
 themeButton.addEventListener("click", changeTheme);
+
+
+
